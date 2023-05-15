7e9a09df82db9623c879469b0b0a4620693fcdd0
Hadi Salameh <hsalameh123@gmail.com>
2023-05-15 10:33:58 -0400

    Added filtering of negative comments

--- pages/api/posts/addComment.ts
+++ pages/api/posts/addComment.ts
@@ -9,20 +9,26 @@ export default async function handler(
     res: NextApiResponse
 ) {
     if (req.method === "POST") {
+        const vader = require("vader-sentiment");
+        const { content, postId } = req.body.data;
         const session = await getServerSession(req, res, authOptions);
         if (!session) {
             return res.status(401).json({ message: "Please sign in to add a comment" })
         }
-
-        const { content, postId } = req.body.data;
-        const currentSessionUser = await prisma.user.findUnique({
-            where: { email: session?.user?.email || undefined }
-        });
-
+      
         if (!content.length) {
             return res.status(400).json({ message: "Please do not leave this empty" })
         }
 
+        const commentScore = vader.SentimentIntensityAnalyzer.polarity_scores(content).compound;
+        if (commentScore < -0.35) {
+            return res.status(403).json({ message: "This comment is inappropriate" })
+        }
+        
+        const currentSessionUser = await prisma.user.findUnique({
+            where: { email: session?.user?.email || undefined }
+        });
+        
         try {
             const result = await prisma.comment.create({
                 data: {
