46f140c1de62bd2610cc86b80aa0b07fc5944c40
Hadi Salameh <hsalameh123@gmail.com>
2023-11-02 22:31:16 -0400

    Temp change

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <QueryWrapper>
           <Nav />
-          {children}
+          {/* {children} */}
         </QueryWrapper>
       </body>
     </html>
