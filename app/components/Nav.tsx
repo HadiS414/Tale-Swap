1bac0d44980083302cbc565047714b7f44ee2ca3
Hadi Salameh <hsalameh123@gmail.com>
2023-11-03 11:20:42 -0400

    Uncommented navbar

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,18 +1,18 @@
-// import Login from "./Login";
-// import LoggedIn from "./LoggedIn";
-// import { getServerSession } from "next-auth/next";
-// import { authOptions } from "../../pages/api/auth/[...nextauth]";
-// import Image from "next/image";
-// import mobileLogo from "../images/TS_mobile_logo.svg";
-// import desktopLogo from "../images/TS_desktop_logo.svg"
-// import Link from "next/link";
+import Login from "./Login";
+import LoggedIn from "./LoggedIn";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
+import Image from "next/image";
+import mobileLogo from "../images/TS_mobile_logo.svg";
+import desktopLogo from "../images/TS_desktop_logo.svg"
+import Link from "next/link";
 
 export default async function Nav() {
-    // const session = await getServerSession(authOptions);
+    const session = await getServerSession(authOptions);
 
     return (
         <nav className="flex py-3 px-6 lg:px-16 sm:mx-10 justify-between items-center">
-            {/* <div className="flex gap-2">
+            <div className="flex gap-2">
                 <Link href={"/"}>
                     <Image
                         src={desktopLogo}
@@ -26,11 +26,11 @@ export default async function Nav() {
                         alt="Mobile logo..."
                     />
                 </Link>
-            </div> */}
-            {/* <div>
+            </div>
+            <div>
                 {!session?.user && <Login />}
                 {session?.user && <LoggedIn image={session.user?.image || ""} email={session.user.email || ""} />}
-            </div> */}
+            </div>
         </nav>
     )
 }
