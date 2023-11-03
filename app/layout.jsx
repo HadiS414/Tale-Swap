31626fb991d7e704c60f3eb479fa1db331f798fb
Hadi Salameh <hsalameh123@gmail.com>
2023-11-02 22:59:36 -0400

    Revert temp change

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
       </head>
       <body className={inter.className}>
         <QueryWrapper>
-          {/* <Nav /> */}
+          <Nav />
           {children}
         </QueryWrapper>
       </body>
