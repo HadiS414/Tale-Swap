865089191741cedb190630f6bbc29eff9b8270a8
Daniel Hamam <danielhamam@outlook.com>
2023-11-03 20:54:10 -0400

    Attempt to hardcode as a replacement for process.env

--- prisma/client.ts
+++ prisma/client.ts
@@ -14,6 +14,6 @@ declare const global: CustomNodeJsGlobal
 
 const prisma = global.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV === "development") global.prisma = prisma
+// if (process.env.NODE_ENV === "development") global.prisma = prisma
 
 export default prisma
