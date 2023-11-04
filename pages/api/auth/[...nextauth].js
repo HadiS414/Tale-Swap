f291d7d1345c4b049f9da96c592ad6dafd5f518a
Daniel Hamam <danielhamam@outlook.com>
2023-11-03 20:48:40 -0400

    Attempt to deploy discarding process.env

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,8 +7,8 @@ export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: "7051258296-2lh31fenvdmuc1686bag9k3uqmo1jqjq.apps.googleusercontent.com",
+      clientSecret: "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",
     }),
   ],
 }
