23336903fa57865cc4f3cd3a120760f4522bd023
Hadi Salameh <hsalameh123@gmail.com>
2024-01-23 22:08:03 -0500

    Added tab icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,18 +11,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="bg-off-white">
+    <>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/images/favicon.ico" />
       </head>
-      <body className={inter.className}>
-        <QueryWrapper>
-          <Nav />
-          {children}
-        </QueryWrapper>
-      </body>
-    </html>
+      <html lang="en" className="bg-off-white">
+        <body className={inter.className}>
+          <QueryWrapper>
+            <Nav />
+            {children}
+          </QueryWrapper>
+        </body>
+      </html>
+    </>
   )
 }
